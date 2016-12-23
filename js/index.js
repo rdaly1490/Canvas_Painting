34a426faf2a72dad279975bee4d460125bf86dd5
Rob <rdaly1490@gmail.com>
2016-12-22 19:48:56 -0600

    make lines change color and width while drawing

--- js/index.js
+++ js/index.js
@@ -6,18 +6,34 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
 	if (!isDrawing) return;
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 	ctx.beginPath();
 	ctx.moveTo(lastX, lastY);
 	ctx.lineTo(e.offsetX, e.offsetY);
 	ctx.stroke();
 	[lastX, lastY] = [e.offsetX, e.offSetY];
+	hue++;
+	if (hue >= 360) hue = 0;
+	controlLineWidth();
+}
+
+function controlLineWidth() {
+	if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) direction = !direction;
+	if (direction) {
+		ctx.lineWidth++;
+	} else {
+		ctx.lineWidth--;
+	}
 }
 
 canvas.addEventListener('mousemove', draw);
